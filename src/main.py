7e3c0cba650851c3144a2ffa84a68a0ec5084af4
Aswin Raj <127181121+aswinsparky@users.noreply.github.com>
2025-11-19 05:45:04 +0000

    change 10

--- src/main.py
+++ src/main.py
@@ -1,26 +1,6 @@
-import os
+def add(a, b):
+    return a + b
 
-def get_user_password():
-    # Hardcoded password (Sensitive Data Exposure)
-    password = "SuperSecret123!"
-    print("Password is:", password)
-
-def read_file():
-    filename = input("Enter filename: ")
-    # Command injection vulnerability
-    os.system(f"cat {filename}")
-
-def unsafe_eval():
-    user_input = input("Enter expression: ")
-    print(eval(user_input))
-
-def bad_hash(data):
-    import hashlib
-    # Insecure hash algorithm for security use
-    return hashlib.md5(data.encode()).hexdigest()
-
-if __name__ == "__main__":
-    get_user_password()
-    read_file()
-    unsafe_eval()
-    print(bad_hash("test123"))
+x = 10
+y = None
+print(x + y)  # type error
